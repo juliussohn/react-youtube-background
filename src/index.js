2fdb8d1f10ba292ba45615e28a4c0e0ab55ae6af
Eedrah <eedrah@users.noreply.github.com>
2018-12-14 00:07:10 +1300

    tidy code with prettier

--- src/index.js
+++ src/index.js
@@ -1,122 +1,121 @@
-import React from 'react';
-import YouTube from 'react-youtube';
-import styles from './styles/Player.css';
+import React from 'react'
+import YouTube from 'react-youtube'
+import styles from './styles/Player.css'
 
 class YoutubeBackground extends React.Component {
+  constructor(props) {
+    super(props)
 
-	constructor(props) {
-		super(props)
+    const aspectRatio = this.props.aspectRatio.split(':')
+    this.state = {
+      aspectRatio: aspectRatio[0] / aspectRatio[1],
+      videoHeight: 10,
+    }
+  }
 
-		const aspectRatio = this.props.aspectRatio.split(':')
+  componentDidMount() {
+    this.updateDimensions()
+    window.addEventListener('resize', this.updateDimensions.bind(this))
+  }
 
-		this.state = {
-			aspectRatio: aspectRatio[0] / aspectRatio[1],
-			videoHeight: 10
-		}
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions.bind(this))
+  }
 
-	}
+  updateDimensions() {
+    const { aspectRatio } = this.state
+    const containerWidth = this.container.clientWidth
+    const containerHeight = this.container.clientHeight
+    const containerAspectRatio = containerWidth / containerHeight
 
-	componentDidMount() {
-		this.updateDimensions();
-		window.addEventListener("resize", this.updateDimensions.bind(this))
-	}
+    let videoHeight = containerHeight
+    let videoWidth = containerWidth
+    let videoY = 0
+    let videoX = 0
 
-	componentWillUnmount() {
-		window.removeEventListener("resize", this.updateDimensions.bind(this))
-	}
+    if (containerAspectRatio > aspectRatio) {
+      videoHeight = containerWidth / aspectRatio
+      videoY = (videoHeight - containerHeight) / -2
+    } else {
+      videoWidth = containerHeight * aspectRatio
+      videoX = (videoWidth - containerWidth) / -2
+    }
 
-	updateDimensions() {
-		const { aspectRatio } = this.state
-		const containerWidth = this.container.clientWidth
-		const containerHeight = this.container.clientHeight
-		const containerAspectRatio = containerWidth / containerHeight
+    this.setState({
+      videoHeight,
+      videoWidth,
+      videoY,
+      videoX,
+    })
+  }
 
-		let videoHeight = containerHeight
-		let videoWidth = containerWidth
-		let videoY = 0
-		let videoX = 0
+  onEnd(event) {
+    event.target.playVideo()
+  }
 
-		if (containerAspectRatio > aspectRatio) {
-			videoHeight = containerWidth / aspectRatio
-			videoY = (videoHeight - containerHeight) / -2
-		} else {
-			videoWidth = containerHeight * aspectRatio
-			videoX = (videoWidth - containerWidth) / -2
-		}
+  onReady(event) {
+    event.target.playVideo()
+    this.props.onReady()
+  }
 
-		this.setState({
-			videoHeight,
-			videoWidth,
-			videoY,
-			videoX
-		});
-	}
+  render() {
+    const { videoHeight, videoWidth, videoX, videoY } = this.state
+    const { style, children, className, overlay } = this.props
+    const playerProps = (({ videoId, onReady }) => ({
+      videoId,
+      onReady,
+    }))(this.props)
 
-	onEnd (event){
-		event.target.playVideo();
-	}
+    const videoOptions = {
+      playerVars: {
+        autoplay: 1,
+        controls: 0,
+        rel: 0,
+        showinfo: 0,
+        mute: 1,
+      },
+    }
 
-	onReady(event){
-		event.target.playVideo();
-		this.props.onReady()
-	}
-
-
-	render() {
-		const { videoHeight, videoWidth, videoX, videoY } = this.state
-		const { style, children, className, overlay } = this.props
-		const playerProps = (({
-			videoId,
-			onReady
-		}) => ({
-			videoId,
-			onReady
-		}))(this.props);
-
-		const videoOptions = {
-			playerVars: { 
-				autoplay: 1,
-				controls: 0,
-				rel: 0,
-				showinfo: 0,
-				mute:1
-			}
-		};
-
-
-		return (
-			<div style={style} ref={c => this.container = c} className={[styles.container,className].join(' ')}>
-				<div>{children}</div>
-				<div
-					className={styles.videoContainer}
-					style={{
-						width: videoWidth + 'px',
-						height: videoHeight + 'px',
-						top: videoY + 'px',
-						left: videoX + 'px'
-					}}
-				>
-					{overlay && 
-					<div className={styles.overlay} style = {{backgroundColor: overlay}}></div>}
-					<YouTube
-						{...playerProps}
-						onEnd={this.onEnd}
-						onReady ={this.onReady.bind(this)}
-						opts={videoOptions}
-						className={styles.videoIframe}
-					></YouTube>
-				</div>
-			</div>
-
-		);
-	}
+    return (
+      <div
+        style={style}
+        ref={c => (this.container = c)}
+        className={[styles.container, className].join(' ')}
+      >
+        <div>{children}</div>
+        <div
+          className={styles.videoContainer}
+          style={{
+            width: videoWidth + 'px',
+            height: videoHeight + 'px',
+            top: videoY + 'px',
+            left: videoX + 'px',
+          }}
+        >
+          {overlay && (
+            <div
+              className={styles.overlay}
+              style={{ backgroundColor: overlay }}
+            />
+          )}
+          <YouTube
+            {...playerProps}
+            onEnd={this.onEnd}
+            onReady={this.onReady.bind(this)}
+            opts={videoOptions}
+            className={styles.videoIframe}
+          />
+        </div>
+      </div>
+    )
+  }
 }
 
 YoutubeBackground.defaultProps = {
-	aspectRatio: '16:9',
-	videoId: 'jssO8-5qmag',
-	overlay: 'false',
-	onReady: ()=>{}
+  aspectRatio: '16:9',
+  videoId: 'jssO8-5qmag',
+  overlay: 'false',
+  onReady: () => {},
 }
 
-export default YoutubeBackground;
+export default YoutubeBackground
